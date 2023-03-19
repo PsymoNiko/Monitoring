f94b7bbdfa751da9e71a4611fdb66cc4c36ca7a3
Ali <psymastercode@gmail.com>
2023-03-19 17:58:54 +0330

    mentor models

--- mentor/models.py
+++ mentor/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+
+class Courses(models.Model):
+    title = models.CharField(max_length=100)
+    course_length = models.IntegerField()
+    completed = models.IntegerField()
+
+class Mentor(models.Model):
+    PERSONALITIES = (
+        'INTP', 'INTJ', 'ENTJ', 'ENTP',
+        'INFJ', 'INFP', 'ENFJ', 'ENFP',
+        'ISTJ', 'ISFJ', ' ESTJ', 'ESFJ',
+        'ISTP', 'ISFP', 'ESTP', 'ESFP',
+    )
+
+    mentor = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=35, editable=False)
+    last_name = models.CharField(max_length=35, editable=False)
+    password = models.CharField(max_length=128)
+    date_of_birth = models.DateField()
+    phone_number = models.CharField(max_length=13, editable=True)
+    identity_code = models.IntegerField(editable=False, unique=True)
+    personality = models.CharField(max_length=4, choices=PERSONALITIES)
+    avatar = models.ImageField(upload_to='profile_images/', blank=True, null=True)
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def courses(self):
+        courses = models.ForeignKey(Courses, on_delete=models.CASCADE)
+
+
+class CurrentCourse(models.Model):
+    courses = models.ForeignKey(Mentor, on_delete=models.CASCADE)
+    course_name = models.CharField(max_length=30)
+    course_length = models.IntegerField()
+    completed = models.IntegerField()
--- student/models.py
+++ student/models.py
@@ -1,25 +1,52 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
-
+from mentor.models import Mentor
 
 
 class Student(models.Model):
-
     PERSONALITIES = (
         'INTP', 'INTJ', 'ENTJ', 'ENTP',
         'INFJ', 'INFP', 'ENFJ', 'ENFP',
-        'ISTJ', 'ISFJ',' ESTJ', 'ESFJ',
+        'ISTJ', 'ISFJ', ' ESTJ', 'ESFJ',
         'ISTP', 'ISFP', 'ESTP', 'ESFP',
     )
-
 
     student = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=35, editable=False)
     last_name = models.CharField(max_length=35, editable=False)
     password = models.CharField(max_length=128)
+    phone_number = models.CharField(max_length=13, editable=True)
     date_of_birth = models.DateField()
     identity_code = models.IntegerField(editable=False, unique=True)
     personality = models.CharField(max_length=4, choices=PERSONALITIES)
     avatar = models.ImageField(upload_to='profile_images/', blank=True, null=True)
+
+
+class CurrentCourse(models.Model):
+    belong_to = models.OneToOneField(Student, on_delete=models.CASCADE)
+    course_name = models.CharField(max_length=30)
+    mentor = models.OneToOneField(Mentor, on_delete=models.CASCADE)
+    course_length = models.IntegerField()
+    completed = models.IntegerField()
+
+
+class CompletedCourses(models.Model):
+    course_name = models.CharField()
+    mentor = models.OneToOneField(Mentor, on_delete=models.CASCADE)
+    course_length = models.IntegerField()
+    started_at = models.DateField()
+    finished_at = models.DateField()
+
+    def course_time(self):
+        return f"{self.started_at} to {self.finished_at}"
+
+
+class StudentSettings(models.Model):
+    """
+    For turn on/off the sms panel for each student
+    """
+    student = models.OneToOneField(Student, on_delete=models.CASCADE)
+    installment_payment_due_date = models.BooleanField(default=False, editable=True)
+    new_task = models.BooleanField(default=False, editable=True)
+    new_message = models.BooleanField(default=False, editable=True)
