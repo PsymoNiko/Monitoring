99f32395706f8f8c56cc66b83ace0152a42a3311
zahra <zahramohammadimehr04@gmail.com>
2023-03-27 13:47:09 +0330

    upload for mahdi

---
+++ dailyreport/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DailyreportConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dailyreport'
---
+++ dailyreport/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+class ExerciseSubmission(models.Model):
+    report_number = models.CharField(max_length=50)
+    date = models.DateTimeField()
+    student_name = models.CharField(max_length=100)
+    total_hours = models.DecimalField(max_digits=6, decimal_places=2)
+    comment = models.TextField(blank=True)
+
+
+#panel student
+from django.db import models
+
+class Report(models.Model):
+    date = models.DateField()
+    report_number = models.IntegerField()
+    caption = models.CharField(max_length=255)
+    hours = models.DecimalField(max_digits=5, decimal_places=2)
+    sent = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.report_number} ({self.date})"
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'date': self.date,
+            'report_number': self.report_number,
+            'caption': self.caption,
+            'hours': self.hours,
+            'sent': self.sent,
+        }
