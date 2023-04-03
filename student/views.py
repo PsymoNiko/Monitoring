710c00b4ba31750c3fb4b176494d3ad1f42d6974
Ali <psymastercode@gmail.com>
2023-04-03 23:47:31 +0330

    correction of databases

--- student/views.py
+++ student/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status, authentication, permissions, generics
+from rest_framework import status, authentication, permissions, generics, viewsets
 
 from rest_framework_simplejwt.views import TokenObtainPairView
 
@@ -8,42 +8,42 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.contrib.auth import authenticate, login
 
-from .models import Student
-from ceo.models import Admin
+from .models import Student, Payment
+# from ceo.models import Admin
 from .serializers import StudentSerializer, StudentTokenObtainPairSerializer,\
-    LoginViewAsStudentSerializer, ReportSerializer
+    LoginViewAsStudentSerializer, ReportSerializer, PaymentSerializer
 
 from django.shortcuts import redirect
 from rest_framework.authtoken.models import Token
 
 
-class CreateStudentView(APIView):
-    @method_decorator(login_required)
-    def post(self, request):
-        # Check if the user is an admin
-        if not request.user.is_staff:
-            return Response({'error': 'You do not have permission to create a student account.'},
-                            status=status.HTTP_403_FORBIDDEN)
-
-        # Get the request data and create the new student account
-        try:
-            admin = Admin.objects.get(user=request.user)
-            student_firstname = request.data['first_name']
-            student_lastname = request.data['last_name']
-            student_phone_number = request.data['phone_number']
-            student_birthday = request.data['date_of_birth']
-            student_identity_code = request.data['identity_code']
-            student_personality = request.data['personality']
-            student_avatar = request.data['avatar']
-            student = admin.create_student_account(student_firstname, student_lastname, student_phone_number,
-                                                   student_birthday, student_identity_code, student_personality,
-                                                   student_avatar)
-        except Exception as e:
-            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
-
-        # Return the new student object
-        serializer = StudentSerializer(student)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+# class CreateStudentView(APIView):
+#     @method_decorator(login_required)
+#     def post(self, request):
+#         # Check if the user is an admin
+#         if not request.user.is_staff:
+#             return Response({'error': 'You do not have permission to create a student account.'},
+#                             status=status.HTTP_403_FORBIDDEN)
+#
+#         # Get the request data and create the new student account
+#         try:
+#             admin = Admin.objects.get(user=request.user)
+#             student_firstname = request.data['first_name']
+#             student_lastname = request.data['last_name']
+#             student_phone_number = request.data['phone_number']
+#             student_birthday = request.data['date_of_birth']
+#             student_identity_code = request.data['identity_code']
+#             student_personality = request.data['personality']
+#             student_avatar = request.data['avatar']
+#             student = admin.create_student_account(student_firstname, student_lastname, student_phone_number,
+#                                                    student_birthday, student_identity_code, student_personality,
+#                                                    student_avatar)
+#         except Exception as e:
+#             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+#
+#         # Return the new student object
+#         serializer = StudentSerializer(student)
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class StudentLoginView(TokenObtainPairView):
@@ -114,3 +114,9 @@ class DailyReportView(generics.CreateAPIView):
             'message': "Report Created successfully",
             'data': serializer.data
         }, status=status.HTTP_201_CREATED)
+
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+
