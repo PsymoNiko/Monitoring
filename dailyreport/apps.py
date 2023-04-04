02f53910c7250b7832d711f190ff32bd67f72124
zahra <zahramohammadimehr04@gmail.com>
2023-04-04 21:04:27 +0330

    new dailyreport and exercise

--- dailyreport/apps.py
+++ dailyreport/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class DailyreportConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dailyreport'
+    def ready(self) -> None:
+        import dailyreport.signals
